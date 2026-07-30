45c5974050e324818cf92d07a5c5e0aa47ffec1c
Sreeram <ramxstudio@gmail.com>
2026-07-30 23:10:32 +0530

    Connect Render Backend

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://online-voting-system-4rvm.onrender.com/api",
 });
 
 export default API;
